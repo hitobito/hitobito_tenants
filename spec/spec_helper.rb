364ec485b76f80eb0618cd9309232ffe177c2ccb
Matthias Viehweger <viehweger@puzzle.ch>
2024-03-06 15:40:44 +0100

    Reset dev-local overrides for specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,7 @@
 load File.expand_path('../../app_root.rb', __FILE__)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 ENV['RAILS_USE_TEST_GROUPS'] = 'true'
+ENV['RAILS_HOST_NAME'] = 'hitobito.local'
 
 require File.join(ENV['APP_ROOT'], 'spec', 'spec_helper.rb')
 
