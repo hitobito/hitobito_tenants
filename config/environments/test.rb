08316a8084f15e3365e969bfb2183aadcb1fa913
Nils Rauch <rauch@puzzle.ch>
2024-09-23 08:04:39 +0200

    Do not eager load test environment because of DB setup

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -2,4 +2,8 @@ Rails.application.configure do
   # DB will always be setup in wagon spec_helper
   # For some reason if we run maintain_test_schema! in the core spec_helper it breaks because of multiple DB accesses
   config.active_record.maintain_test_schema = false
+  
+
+  # Do not eager load because of DB setup getting screwed
+  config.eager_load = false
 end
