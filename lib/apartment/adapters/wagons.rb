4c55d960b0b3a89cb6dc1511b75ed73eec3153ff
MarcEgliP <88040929+MarcEgliP@users.noreply.github.com>
2024-11-27 09:52:01 +0100

    feature/rails-update-7.0 (#20)
    Rails update v7.0

--- lib/apartment/adapters/wagons.rb
+++ lib/apartment/adapters/wagons.rb
@@ -20,6 +20,8 @@ module Apartment
           migrate_core
           migrate_wagons
           Person.reset_column_information
+          CustomContent::Translation.reset_column_information
+          MailLog.reset_column_information
           seed # core
           seed_wagons
         end
---
+++ spec/support/reload_column_information.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+CustomContent::Translation.reset_column_information
+CustomContent.reset_column_information
+MailLog.reset_column_information
