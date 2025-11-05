d2a566d005061588e0baaa4741b10dca2bd3de6a
Philipp Koster <koster@puzzle.ch>
2025-11-05 14:06:48 +0100

    Migrate from raven to sentry gem

--- app/controllers/tenants/application_controller.rb
+++ app/controllers/tenants/application_controller.rb
@@ -2,10 +2,12 @@ module Tenants::ApplicationController
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_sentry_tenant_context
+    before_action :set_sentry_tenant_tag
   end
 
-  def set_sentry_tenant_context
-    Raven.tags_context(tenant: Apartment::Tenant.current)
+  def set_sentry_tenant_tag
+    Sentry.configure_scope do |scope|
+      scope.set_tags(tenant: Apartment::Tenant.current)
+    end
   end
 end
