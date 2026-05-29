c64203d89f4060f34e1509274ae8ecb302ad2e0d
Andreas Maierhofer <a.maierhofer@gmail.com>
2026-05-29 14:48:51 +0200

    Configure id_prefix_addition for pass services (hitobito/hitobito#3911)

---
+++ spec/domain/wallets/apple/pass_service_spec.rb
@@ -0,0 +1,36 @@
+#  Copyright (c) 2026, Puzzle ITC. This file is part of
+#  hitobito and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito.
+
+require "spec_helper"
+
+describe Wallets::AppleWallet::PassService do
+  let(:person) { Fabricate(:person) }
+  let(:definition) { Fabricate(:pass_definition) }
+
+  let(:pass) do
+    Fabricate(:pass, person: person, pass_definition: definition,
+      state: :eligible, valid_from: Date.current)
+  end
+  let(:installation) { Fabricate(:wallets_pass_installation, pass: pass, wallet_type: :apple) }
+
+  let(:client) { instance_double(Wallets::AppleWallet::PkpassGenerator) }
+
+  subject(:service) do
+    described_class.new(installation, client: client)
+  end
+
+  before do
+    allow(Wallets::AppleWallet::Config).to receive(:pass_type_identifier).and_return("pass.com.example.test")
+    allow(Wallets::AppleWallet::Config).to receive(:team_identifier).and_return("ABCDE12345")
+    allow(Wallets::AppleWallet::Config).to receive(:web_service_url).and_return("https://example.com/api/apple")
+  end
+
+
+  it "includes tenant in id prefix" do
+    Apartment::Tenant.switch("cool-people-foundation") do
+      expect(service.pass_data[:serialNumber]).to eq("hitobito.cool-people-foundation.#{installation.id}")
+    end
+  end
+end
